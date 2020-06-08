e82ece773901465c70826540a6a6171352fed168
theTrainMan <stormbreaker70@gmail.com>
2020-06-08 13:14:33 +0200

    Updates

---
+++ precache-manifest.acd2286f010681ba2dd026fc6130b474.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89e5a7380a47d8ef327f767946773fea",
+    "url": "/theTrainMan/colors-ui.git/index.html"
+  },
+  {
+    "revision": "12368e49782cdd118566",
+    "url": "/theTrainMan/colors-ui.git/static/css/main.12535b3a.chunk.css"
+  },
+  {
+    "revision": "f92be88c6ef53582cf65",
+    "url": "/theTrainMan/colors-ui.git/static/js/2.9e1a4cd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/theTrainMan/colors-ui.git/static/js/2.9e1a4cd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12368e49782cdd118566",
+    "url": "/theTrainMan/colors-ui.git/static/js/main.ac1af9e6.chunk.js"
+  },
+  {
+    "revision": "15bbb757694c677023c7",
+    "url": "/theTrainMan/colors-ui.git/static/js/runtime-main.2285fe00.js"
+  }
+]);
